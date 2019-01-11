643de2d9c843eee78a9b6a10995050cb4eb71620
Arun Kumar Palani <arunpala@cisco.com>
2019-01-11 20:48:07 +0530

    Front End with Ag-Grid

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      columnDefs: [
+        { headerName: "Id", field: "id" },
+        { headerName: "Timestamp", field: "timestamp" },
+        { headerName: "Type", field: "type" },
+        { headerName: "Message", field: "message" }
+      ],
+      defaultColDef: {
+        width: 120,
+        suppressFilter: true
+      }
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://frontend:8080/aknms/v1/event/')
+      .then(result => result.json())
+      .then(rowData => this.setState({ rowData }))
+  }
+
+  render() {
+    return (
+      <div
+        className="ag-theme-balham"
+        style={{
+          height: '500px',
+          width: '600px'
+        }}
+      >
+        <AgGridReact
+          enableSorting={true}
+          enableFilter={true}
+          columnDefs={this.state.columnDefs}
+          rowData={this.state.rowData}>
+        </AgGridReact>
+      </div>
+    );
+  }
+}
+
+export default App;
