3445b9211e53b7370ea6e6df7c9dcc4e9f62e3dc
Karthik Rajagopalan <karthraj@cisco.com>
2019-01-24 10:48:56 +0530

    With infinite scrool

---
+++ src/components/RefreshPoller.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class RefreshPoller extends React.Component {
+    
+    
+    render(){
+        return (
+            <div>
+                <h1>Hello, RefreshPoller</h1>
+            </div>
+        );
+	}
+    
+}
+export default RefreshPoller;
